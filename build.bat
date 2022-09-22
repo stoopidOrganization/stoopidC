e4ce28d2cdf9843a12d953e7d15d82172d12245b
nils865 <nils.staas0806@gmail.com>
2022-09-22 08:59:50 +0200

    improved build script

--- build.bat
+++ build.bat
@@ -1,26 +1,21 @@
 @echo off
+del .\build\* /q
+rmdir .\build\
 mkdir build
 
 echo compiling scripts...
 cd .\src\
-gcc -Wall -c lib.c
-gcc -Wall -c global.c
-gcc -Wall -c variables.c
-gcc -Wall -c main.c
+gcc -Wall -c *.c
 echo compiled scripts
 echo:
 
 echo building exe...
 cd ..\build\
-gcc -o stoopid ..\src\lib.o ..\src\global.o ..\src\variables.o ..\src\main.o
+gcc -o stoopid ..\src\*.o
 echo build finished
 echo:
 
-cd ..\src\
-del lib.o
-del main.o
-del global.o
-del variables.o
+del ..\src\*.o
 echo successfully build exe
 echo:
 @echo on
