059e6d3a34d756cefb3184e47dd4fae93eaebef4
nils865 <nils.staas0806@gmail.com>
2022-10-23 13:10:45 +0200

    basic infrastructure for new math - very basic

--- src/math.c
+++ src/math.c
@@ -35,60 +35,74 @@ double solveSimpleEquasion(double num1, char operator ,double num2) {
     return 0;
 }
 
+double getNum1(char* equasion, size_t operator) {
+    double num1 = 0;
+    int start = operator - 1;
+    printf("%d\n", start);
+
+    while(!isOperator(equasion[start - 1]) && start > 0) start--;
+
+    printf("%d\n", start);
+
+    return num1;
+}
+
 char* solveComplexEquasion(char* equasion) {
     char* finishedEquasion = equasion;
-    int numLimit = 1024;
+    // int numLimit = 1024;
+    bool resolving = true;
 
-    // resolve stage 1 operation
-    for(int i = 0; i < getSize(equasion); i++) {
-        if(isOperator(finishedEquasion[i])) {
-            // get first number
-            int num1Start = 0;
-            for(int j = i - 1; j >= -1; j--) {
-                if(isOperator(finishedEquasion[j])) {
-                    num1Start = j + 1;
-                }
+    while(resolving) {
+        // test if equasion is finished
+        bool done = true;
+        bool stage1 = false;
+        bool stage2 = false;
+        bool stage3 = false;
+
+        for(size_t i = 0; i < getSize(finishedEquasion); i++) {
+            if(isOperator(finishedEquasion[i])) {
+                done = false;
             }
-            char* num1AsStr = malloc(numLimit * sizeof(char));
-            int k = 0;
-            for(int j = num1Start; j < i; j++) {
-                num1AsStr[k] = finishedEquasion[j];
-                k++;
-            }
-            num1AsStr[k] = '\0';
-
-            double num1 = strToDouble(num1AsStr);
-
-            free(num1AsStr);
-
-            // get second number
-            int end;
-            char* num2AsStr = malloc(numLimit * sizeof(char));
-            k = 0;
-            for(int j = i + 1; j < getSize(finishedEquasion); j++) {
-                if(isOperator(finishedEquasion[j]) || finishedEquasion[j] == '\0') {
-                    end = j;
-                    break;
-                }
-
-                num2AsStr[k] = finishedEquasion[j];
-            }
-            num2AsStr[k + 1] = '\0';
-            
-            double num2 = strToDouble(num2AsStr);
-
-            free(num2AsStr);
-
-            char* result = malloc(numLimit);
             
             if(finishedEquasion[i] == '^') {
-                sprintf(result, "%f", solveSimpleEquasion(num1, '^', num2));
+                stage1 = true;
+            } else if(finishedEquasion[i] == '*' || finishedEquasion[i] == '/' || finishedEquasion[i] == '%') {
+                stage2 = true;
+            } else if(finishedEquasion[i] == '+' || finishedEquasion[i] == '-') {
+                stage3 = true;
             }
-            
-            finishedEquasion = replaceString(equasion, result, 0, end);
-
-            return solveComplexEquasion(finishedEquasion);
         }
+
+        if(done) {
+            resolving = false;
+            break;
+        }
+        
+        for(size_t i = 0; i < getSize(finishedEquasion); i++) {
+            if(isOperator(finishedEquasion[i])) {
+                getNum1(finishedEquasion, i);
+
+                // resolving the equasion
+                if(stage1) {
+                    // TODO implement '^' here
+                    printf("stage1");
+                    resolving = false;
+                    break;
+                } else if(stage2 && !stage1) {
+                    // TODO implement '*', '/', '%' here
+                    printf("stage2");
+                    resolving = false;
+                    break;
+                } else if(stage3 && !stage1 && !stage2) {
+                    // TODO implement '+', '-' here
+                    printf("stage3");
+                    resolving = false;
+                    break;
+                }
+            }
+        }
+        
+        
     }
 
     return finishedEquasion;
