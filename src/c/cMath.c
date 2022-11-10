0335b32fd16889879f94c9eb1ca18fb914b63113
Nils <nils.staas0806@gmail.com>
2022-11-10 23:24:31 +0100

    fixed variables not working with math

--- src/c/cMath.c
+++ src/c/cMath.c
@@ -10,7 +10,7 @@ int isOperator(char op) {
 }
 
 int isCharNum(char num) {
-    if (num == '-' || num == '0' || num == '1' || num == '2' || num == '3' || num == '4' || num == '5' || num == '6' || num == '7' || num == '8' || num == '9') {
+    if (num == '-' || num == '.' || num == '0' || num == '1' || num == '2' || num == '3' || num == '4' || num == '5' || num == '6' || num == '7' || num == '8' || num == '9') {
         return 1;
     }
 
