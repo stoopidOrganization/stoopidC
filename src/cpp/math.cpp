7d2703d06b54e4045270ebe4055d30b40714ba81
Nils <nils.staas0806@gmail.com>
2022-11-10 15:27:14 +0100

    more refactoring

--- src/cpp/math.cpp
+++ src/cpp/math.cpp
@@ -1,11 +1,14 @@
 #include <string>
 #include <queue>
 #include <vector>
+#include <iostream>
 
-double solveEquasion(std::string equasion) {
-    // std::queue<std::string> equasionInRPN = convertToRPN(splitEquasion(equasion));
+std::vector<std::string> splitEquasion(std::string equasion) {
+    std::vector<std::string> output;
 
-    return 0.1;
+    output.push_back("test");
+
+    return output;
 }
 
 std::queue<std::string> convertToRPN(std::vector<std::string> equasion) {
@@ -14,8 +17,16 @@ std::queue<std::string> convertToRPN(std::vector<std::string> equasion) {
     return output;
 }
 
-std::vector<std::string> splitEquasion(std::string equasion) {
-    std::vector<std::string> output;
+double solveEquasion(std::string equasion) {
+    // std::queue<std::string> equasionInRPN = convertToRPN(splitEquasion(equasion));
 
-    return output;
+    std::vector<std::string> splitUp = splitEquasion(equasion);
+
+    for (size_t i = 0; i < splitUp.size(); i++) {
+        std::string current = splitUp[i];
+
+        std::cout << current << std::endl;
+    }
+
+    return 0.1;
 }
--- src/cpp/math.hpp
+++ src/cpp/math.hpp
@@ -1,3 +1,4 @@
 #include <string>
+#include <vector>
 
 double solveEquasion(std::string equasion);
