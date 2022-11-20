1785390393834da944cefaf266158d0d9f3a3443
Nils <nils.staas0806@gmail.com>
2022-11-20 15:33:54 +0100

    improved error message display

--- src/cpp/exception/exceptionHandler.cpp
+++ src/cpp/exception/exceptionHandler.cpp
@@ -4,10 +4,14 @@
 #include "../../main.hpp"
 
 void throwException(std::string msg) {
+    std::cout << "\n-----------------------------------\n" << std::endl;
+
     std::string output = "The Interpreter crashed in line ";
     output += std::to_string(getCurrentLine() + 1);
     output += ":\n\t";
     output += msg;
 
     std::cerr << output << std::endl;
+
+    std::cout << "\n-----------------------------------\n" << std::endl;
 }
